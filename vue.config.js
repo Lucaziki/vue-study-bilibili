3aa5f313a34948c7173dc5cbb0d2055c1e4f9f65
Lucaziki <Lucaziki@users.noreply.github.com>
2022-05-01 17:03:02 +0800

    获取b站频道的categories数据

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue-cli配置文件，90%的配置都是webpack配置
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      // 配置代理
+      "/x": {
+        //凡是以/x开头的请求，进行代理
+        target: "https://api.bilibili.com",
+        onProxyReq(proxyReq) {
+          proxyReq.setHeader("origin", "https://www.bilibili.com");
+          proxyReq.setHeader("referer", "https://www.bilibili.com/v/channel");
+        },
+      },
+    },
+  },
+};
